8383060fbfec8039102653a208c2b907b139e4bf
Marek Kohn <me@makohn.de>
2025-12-21 09:12:00 +0100

    Optimize solution for 2024 day 7

--- 2024/src/Day07.kt
+++ 2024/src/Day07.kt
@@ -8,27 +8,28 @@ class Day07 : Day<Long, Long>(year = 2024, day = 7) {
             val values = line.extractLongs()
             val testValue = values[0]
             val numbers = values.drop(1)
-            val results = ArrayList<Long>()
-            results.add(numbers[0])
-            outer@ for (i in 1..<numbers.size) {
-                val res = ArrayList(results)
-                results.clear()
-                for (r in res) {
-                    val add = r + numbers[i]
-                    val mul = r * numbers[i]
-                    if (i == numbers.lastIndex && (add == testValue || mul == testValue)) {
-                        sum += testValue
-                        break@outer
-                    }
-                    if (add <= testValue) results.add(add)
-                    if (mul <= testValue) results.add(mul)
-                }
+            fun solve(n: Long, i: Int): Boolean = when {
+                i == numbers.size -> n == testValue
+                n > testValue -> false
+                solve(n + numbers[i], i + 1) -> true
+                solve(n * numbers[i], i + 1) -> true
+                else -> false
             }
+
+            if(solve(numbers[0], 1)) sum += testValue
         }
         return sum
     }
 
-    private fun Long.concat(other: Long) = (this.toString() + other.toString()).toLong()
+    private fun Long.concat(other: Long): Long {
+        var o = other
+        var t = this
+        while (o > 0) {
+            t *= 10
+            o /= 10
+        }
+        return t + other
+    }
 
     override fun part2(input: String): Long {
         var sum = 0L
@@ -36,24 +37,17 @@ class Day07 : Day<Long, Long>(year = 2024, day = 7) {
             val values = line.extractLongs()
             val testValue = values[0]
             val numbers = values.drop(1)
-            val results = ArrayList<Long>()
-            results.add(numbers[0])
-            outer@ for (i in 1..<numbers.size) {
-                val res = ArrayList(results)
-                results.clear()
-                for (r in res) {
-                    val add = r + numbers[i]
-                    val mul = r * numbers[i]
-                    val concat = r.concat(numbers[i])
-                    if (i == numbers.lastIndex && (add == testValue || mul == testValue || concat == testValue)) {
-                        sum += testValue
-                        break@outer
-                    }
-                    if (add <= testValue) results.add(add)
-                    if (mul <= testValue) results.add(mul)
-                    if (concat <= testValue) results.add(concat)
-                }
+
+            fun solve(n: Long, i: Int): Boolean = when {
+                i == numbers.size -> n == testValue
+                n > testValue -> false
+                solve(n + numbers[i], i + 1) -> true
+                solve(n * numbers[i], i + 1) -> true
+                solve(n.concat(numbers[i]), i + 1) -> true
+                else -> false
             }
+
+            if(solve(numbers[0], 1)) sum += testValue
         }
         return sum
     }
