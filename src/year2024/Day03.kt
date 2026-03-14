26eae382a400a9201b0c91e94cab2e44a04f5d45
Marek Kohn <me@makohn.de>
2026-03-14 19:00:00 +0100

    Refactor solution for 2024 day 3

--- src/year2024/Day03.kt
+++ src/year2024/Day03.kt
@@ -2,35 +2,60 @@ package year2024
 
 import util.core.*
 
-class Day03() : Solution<Int, Int>(year = 2024, day = 3) {
+class Day03 : Solution<Int, Int>(year = 2024, day = 3) {
+
+    private fun solve(input: String, ignoreEnabled: Boolean): Int {
+        var i = 0
+        fun nextNumber(): Int {
+            var n = 0
+            while (input[i].isDigit()) {
+                n = 10 * n + input[i].digitToInt()
+                i++
+            }
+            return n
+        }
+
+        var ans = 0
+        var enabled = true
+        while (i < input.length) {
+            when {
+                input[i] != 'm' && input[i] != 'd' -> {
+                    i++
+                    continue
+                }
+                input.startsWith("mul(", i) -> i += 4
+                input.startsWith("do()", i) -> {
+                    i += 4
+                    enabled = true
+                    continue
+                }
+
+                input.startsWith("don't()", i) -> {
+                    i += 7
+                    enabled = false
+                    continue
+                }
+
+                else -> i++
+            }
+            val a = nextNumber()
+            if (input[i] != ',') continue
+            i++
+            val b = nextNumber()
+            if (input[i] != ')') continue
+            i++
+            val res = a * b
+            ans += if (ignoreEnabled || enabled) res else 0
+        }
+        return ans
+    }
 
     override fun part1(input: String): Int {
-        return Regex("mul\\((\\d+),(\\d+)\\)")
-            .findAll(input)
-            .map { it.groupValues }
-            .sumOf { it[1].toInt() * it[2].toInt() }
+        return solve(input, true)
     }
 
     override fun part2(input: String): Int {
-        var acc = 0
-        var enabled = true
-        var inp: String
-        for (i in input.indices) {
-            inp = input.substring(i, input.length)
-            if (inp.startsWith("do()")) {
-                enabled = true
-            }
-            if (inp.startsWith("don't()")) {
-                enabled = false
-            }
-            val res = Regex("mul\\((\\d+),(\\d+)\\)").matchAt(inp, 0)?.groupValues
-            res?.let {
-                if (enabled) {
-                    acc += it[1].toInt() * it[2].toInt()
-                }
-            }
-        }
-        return acc
+        return solve(input, false)
     }
 }
 
