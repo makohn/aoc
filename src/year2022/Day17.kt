1c12ed1d26e8219e3ff4f80ed92060be8e036719
Marek Kohn <me@makohn.de>
2026-03-27 13:25:07 +0100

    Add solution for 2022 day 17 part 2

--- src/year2022/Day17.kt
+++ src/year2022/Day17.kt
@@ -2,7 +2,7 @@ package year2022
 
 import util.core.*
 
-class Day17 : Solution<Int, Int>(year = 2022, day = 17) {
+class Day17 : Solution<Int, Long>(year = 2022, day = 17) {
 
     private data class Rock(val shape: Int, val height: Int)
 
@@ -45,7 +45,63 @@ class Day17 : Solution<Int, Int>(year = 2022, day = 17) {
         return height
     }
 
-    override fun part2(input: String): Int {
-        return 0
+    override fun part2(input: String): Long {
+        var height = 0
+        val tower = IntArray(13000)
+        val seen = HashSet<Int>()
+        var repeatIndex: Int? = null
+        var numRocks = 0L
+        var bottomHeight = 0
+        var repeatHeight = 0
+        var repeatNumRocks = 0L
+        var j = 0
+        tower[0] = 0xff
+        var k = -1L
+        val totalRocks = 1000000000000
+        var n = totalRocks
+        while (++k < n) {
+            val ri = (k % 5).toInt()
+            val rock = ROCKS[ri]
+            var shape = rock.shape
+            var collider = BORDER
+            var pos = height + 3
+            var jetWrapped = false
+            while (true) {
+                val jet = input[j]
+                j = (j + 1) % input.length
+                if (j == 0) jetWrapped = true
+                val pushedShape = if (jet == '<') shape.rotateLeft(1) else shape.rotateRight(1)
+                if (pushedShape and collider == 0) shape = pushedShape
+                collider = (collider shl 8) or BORDER or tower[pos]
+                if (shape and collider != 0) {
+                    for (j in 0..3) {
+                        val mask = tower[pos + j + 1]
+                        tower[pos + j + 1] = mask or (shape shr 8 * j) and 0xff
+                    }
+                    height = maxOf(height, pos + rock.height)
+                    break
+                } else pos--
+            }
+            val rii = (ri + 1) % 5
+            if (rii == 0 && jetWrapped) {
+                if (repeatIndex == null) {
+                    if (j in seen) {
+                        numRocks = k
+                        bottomHeight = height
+                        repeatIndex = j
+                    } else {
+                        seen += j
+                    }
+                } else if (repeatIndex == j) {
+                    repeatHeight = height - bottomHeight
+                    repeatNumRocks = k - numRocks
+                    n = k + (totalRocks - numRocks) % repeatNumRocks
+                }
+            }
+        }
+        val modHeight = height - bottomHeight - repeatHeight
+        val repeatCount = if (repeatNumRocks > 0) (totalRocks - numRocks) / repeatNumRocks else 0
+        val ans = bottomHeight + repeatCount * repeatHeight + modHeight
+        return ans
     }
 }
