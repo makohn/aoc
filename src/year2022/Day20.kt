5ab2633c499888a8f3d9aecf279e3fb514fcb856
Marek Kohn <me@makohn.de>
2026-03-29 11:15:54 +0200

    Optimize solution for 2022 day 20
    Use block decomposition

--- src/year2022/Day20.kt
+++ src/year2022/Day20.kt
@@ -2,45 +2,60 @@ package year2022
 
 import util.core.*
 import util.parse.*
+import kotlin.math.sqrt
 
-class Day20 : Solution<Int, Long>(year = 2022, day = 20) {
+class Day20 : Solution<Long, Long>(year = 2022, day = 20) {
 
-    private data class Number<T>(val value: T, val index: Int)
+    private fun mix(values: List<Long>, times: Int = 1): Long {
+        val n = values.size
+        val blockSize = sqrt(n.toDouble()).toInt() + 1
+        val numBlocks = (n + blockSize - 1) / blockSize
 
-    private inline fun <T> mix(numbers: MutableList<Number<T>>, n: Int, times: Int, offset: (T) -> Int) {
-        val lookup = IntArray(n) { it }
+        val blocks = MutableList(numBlocks) { mutableListOf<Int>() }
+        val blockOf = IntArray(n) { i ->
+            val b = i / blockSize
+            blocks[b].add(i)
+            b
+        }
+
         repeat(times) {
-            for (i in numbers.indices) {
-                val index = lookup[i]
-                val number = numbers[index]
-                val offset = offset(number.value)
-                val expected = (index + offset + (n - 1)) % (n - 1)
-                numbers.removeAt(index)
-                numbers.add(expected, number)
-                for (j in numbers.indices) {
-                    lookup[numbers[j].index] = j
+            for (i in 0..<n) {
+                val value = values[i]
+                if (value % (n - 1) == 0L) continue
+                val block = blockOf[i]
+                val innerPos = blocks[block].indexOf(i)
+                blocks[block].removeAt(innerPos)
+
+                val pos = blocks.take(block).sumOf { it.size } + innerPos
+                val steps = (value % (n - 1)).toInt()
+                var newPos = (pos + steps) % (n - 1)
+                if (newPos < 0) newPos += (n - 1)
+
+                var targetBlock = 0
+                var p = newPos
+                while (targetBlock < blocks.size && p >= blocks[targetBlock].size) {
+                    p -= blocks[targetBlock].size
+                    targetBlock++
                 }
+                blocks[targetBlock].add(p, i)
+                blockOf[i] = targetBlock
             }
         }
+
+        val order = blocks.flatten()
+        val zeroIndex = order.indexOfFirst { values[it] == 0L }
+        fun get(offset: Int) = values[order[(zeroIndex + offset) % n]]
+
+        return get(1000) + get(2000) + get(3000)
     }
 
-    override fun part1(input: String): Int {
-        val numbers = input.extractInts().withIndex().map { (index, value) -> Number(value, index) }.toMutableList()
-        val n = numbers.size
-        mix(numbers, n, 1) { it % (n - 1) }
-        val zeroIndex = numbers.indexOfFirst { it.value == 0 }
-        return numbers[(1000 + zeroIndex) % n].value +
-                numbers[(2000 + zeroIndex) % n].value +
-                numbers[(3000 + zeroIndex) % n].value
+    override fun part1(input: String): Long {
+        val numbers = input.extractLongs()
+        return mix(numbers)
     }
 
     override fun part2(input: String): Long {
-        val numbers = input.extractInts().withIndex().map { (index, value) -> Number(811589153L * value, index) }.toMutableList()
-        val n = numbers.size
-        mix(numbers, n, 10) { (it % (n - 1)).toInt() }
-        val zeroIndex = numbers.indexOfFirst { it.value == 0L }
-        return numbers[(1000 + zeroIndex) % n].value +
-                numbers[(2000 + zeroIndex) % n].value +
-                numbers[(3000 + zeroIndex) % n].value
+        val numbers = input.extractLongs().map { 811589153L * it }
+        return mix(numbers, 10)
     }
 }
