2407ed6cc3ce0d6d85271a16cbf469455c2342b8
Marek Kohn <me@makohn.de>
2026-04-14 21:12:15 +0200

    Read inputs lazily, remove test input property

--- src/util/core/Core.kt
+++ src/util/core/Core.kt
@@ -13,8 +13,7 @@ const val LOCAL_DATA_DIR = "data"
  * @param U the return type of the part 2 solution
  */
 abstract class Solution<T, U>(val year: Int, val day: Int) {
-    val input = readInput(year, day)
-    val testInput by lazy { readTestInput(year, day) }
+    val input by lazy { readInput(year, day) }
 
     abstract fun part1(input: String): T
     abstract fun part2(input: String): U
