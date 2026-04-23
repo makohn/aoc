638f0873c2d7753db9faf6af8bddf391b662b59f
Marek Kohn <me@makohn.de>
2026-04-23 21:10:44 +0200

    Refactor solution for 2020 day 6

--- src/year2020/Day06.kt
+++ src/year2020/Day06.kt
@@ -4,12 +4,17 @@ import util.core.*
 
 class Day06 : Solution<Int, Int> {
 
-    override fun part1(input: String): Int = input.split("\n\n").sumOf { group -> (group.toSet() - '\n').size }
+    private fun count(group: String, operation: (Int, Int) -> Int) = group
+        .lines()
+        .map { it.fold(0) { a, b -> a or (1 shl (b - 'a')) } }
+        .reduce(operation)
+        .countOneBits()
+
+    override fun part1(input: String): Int = input
+        .split("\n\n")
+        .sumOf { count(it) { a, b -> a or b } }
 
     override fun part2(input: String): Int = input
         .split("\n\n")
-        .sumOf { group ->
-            group.lines().map { line -> line.fold(0) { a, b -> a or (1 shl (b - 'a')) } }.reduce { a, b -> a and b }
-                .countOneBits()
-        }
+        .sumOf { count(it) { a, b -> a and b } }
 }
