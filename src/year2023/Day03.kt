10dc7ae0a733629deabc19219ac0bbeeccb18cdc
Marek Kohn <me@makohn.de>
2026-05-02 20:17:11 +0200

    Refactor solution for 2023 day 3

--- src/year2023/Day03.kt
+++ src/year2023/Day03.kt
@@ -2,82 +2,102 @@ package year2023
 
 import util.core.*
 import util.grid.*
-import util.math.product
+import util.point.*
 
 class Day03 : Solution<Int, Int> {
 
-    fun Char.isSymbol(): Boolean = !this.isDigit() && this != '.'
+    private class Input(
+        val grid: CharGrid,
+        val seen: IntGrid,
+        val parts: ArrayList<Int>,
+    ) {
+        operator fun component1(): CharGrid = grid
+        operator fun component2(): IntGrid = seen
+        operator fun component3(): ArrayList<Int> = parts
+    }
 
-    override fun part1(input: String): Int {
-        val schematic = input.lines().toCharGrid()
-        val partNumbers = ArrayList<Int>()
+    private fun parse(input: String): Input {
+        val grid = input.lines().toCharGrid()
+        val (width, height) = grid.shape
+        val seen = IntGrid(height, width) { 0 }
+        val parts = arrayListOf(0)
+        var number = 0
 
-        var number = ""
-        var isPartNumber = false
+        for (y in 0..<height) {
+            for (x in 0..<width) {
+                val position = Point(x, y)
+                val char = grid[position]
 
-        fun reset() {
-            if (number.isNotEmpty() && isPartNumber) {
-                partNumbers.add(number.toInt())
-            }
-            number = ""
-            isPartNumber = false
-        }
-
-        for ((i, row) in schematic.withIndex()) {
-            for ((j, char) in row.withIndex()) {
-                when (char) {
-                    in '0'..'9' -> {
-                        number += char
-                        if (schematic.neighborsOf(i, j).any { it.data.isSymbol() }) isPartNumber = true
-                    }
-                    else -> reset()
+                if (char.isDigit()) {
+                    seen[position] = parts.size
+                    number = 10 * number + char.digitToInt()
+                } else if (number > 0) {
+                    parts.add(number)
+                    number = 0
                 }
             }
-            reset()
+            if (number > 0) {
+                parts.add(number)
+                number = 0
+            }
         }
 
-        return partNumbers.sum()
+        return Input(grid, seen, parts)
+    }
+
+    override fun part1(input: String): Int {
+        val (grid, seen, parts) = parse(input)
+        val (width, height) = grid.shape
+        var result = 0
+
+        for (y in 0..<height) {
+            for (x in 0..<width) {
+                val position = Point(x, y)
+                val char = grid[position]
+
+                if (!char.isDigit() && char != '.') {
+                    for (direction in DIRS_8) {
+                        val index = seen[position + direction]
+                        if (index != 0) {
+                            result += parts[index]
+                            parts[index] = 0
+                        }
+                    }
+                }
+            }
+        }
+        return result
     }
 
     override fun part2(input: String): Int {
-        val schematic = input.lines().toCharGrid()
-        val gearPositions = HashSet<CharPoint>()
-        val gearCandidates = HashMap<CharPoint, MutableList<Int>>()
+        val (grid, seen, parts) = parse(input)
+        val (width, height) = grid.shape
+        var result = 0
 
-        var number = ""
+        for (y in 0..<height) {
+            for (x in 0..<width) {
+                val position = Point(x, y)
 
-        fun reset() {
-            if (number.isNotEmpty()) {
-                for (pos in gearPositions) {
-                    gearCandidates.putIfAbsent(pos, ArrayList())
-                    gearCandidates[pos]?.add(number.toInt())
-                }
-                number = ""
-                gearPositions.clear()
-            }
-        }
+                if (grid[position] == '*') {
+                    var previous = 0
+                    var distinct = 0
+                    var subtotal = 1
 
-        for ((i, row) in schematic.withIndex()) {
-            for ((j, char) in row.withIndex()) {
-                when (char) {
-                    in '0'..'9' -> {
-                        number += char
-                        gearPositions.addAll(
-                            schematic
-                                .neighborsOf(i, j)
-                                .filter { it.data == '*' }
-                                .toList(),
-                        )
+                    for (direction in DIRS_8) {
+                        val index = seen[position + direction]
+                        if (index != 0 && index != previous) {
+                            previous = index
+                            distinct += 1
+                            subtotal *= parts[index]
+                        }
                     }
-                    else -> reset()
+
+                    if (distinct == 2) {
+                        result += subtotal
+                    }
                 }
             }
-            reset()
         }
-
-        return gearCandidates
-            .map { it.value }
-            .filter { it.size == 2 }
-            .sumOf { it.product() }
+        return result
     }
 }
