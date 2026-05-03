39f56a6fbdc98b2657f02a8e1b88bd184d5d9093
Marek Kohn <me@makohn.de>
2026-05-03 10:27:41 +0200

    Refactor solution for 2023 day 10

--- src/year2023/Day10.kt
+++ src/year2023/Day10.kt
@@ -1,82 +1,59 @@
 package year2023
 
-import util.algorithm.bfs
 import util.core.*
 import util.grid.*
+import util.point.*
+import kotlin.math.abs
 
 class Day10 : Solution<Int, Int> {
 
-    fun getNeighbours(input: String): Pair<CharPoint, Map<CharPoint, List<CharPoint>>> {
+    private fun determinant(a: Point, b: Point): Int = a.x * b.y - a.y * b.x
+
+    private inline fun parse(input: String, area: (Point, Point) -> Unit = { _, _ -> }): Int {
         val grid = input.lines().toCharGrid()
-        val (width, height) = grid.shape
 
-        lateinit var startCell: CharPoint
-        val neighbours = mutableMapOf<CharPoint, List<CharPoint>>()
+        var corner = grid.positionOf('S')
+        var direction = if (grid[corner] in "|7F") UP else DOWN
+        val position = corner + direction
+        var steps = 1
 
-        for (y in 0..<height) {
-            for (x in 0..<width) {
-                val currentCell = CharPoint(y, x, grid[y][x])
-                when (currentCell.data) {
-                    '|' -> neighbours[currentCell] = grid.neighborsOf(currentCell, Direction.North, Direction.South)
-                    '-' -> neighbours[currentCell] = grid.neighborsOf(currentCell, Direction.East, Direction.West)
-                    'L' -> neighbours[currentCell] = grid.neighborsOf(currentCell, Direction.North, Direction.East)
-                    'J' -> neighbours[currentCell] = grid.neighborsOf(currentCell, Direction.North, Direction.West)
-                    '7' -> neighbours[currentCell] = grid.neighborsOf(currentCell, Direction.South, Direction.West)
-                    'F' -> neighbours[currentCell] = grid.neighborsOf(currentCell, Direction.South, Direction.East)
-                    'S' -> startCell = currentCell
+        while (true) {
+            while (grid[position] == '-' || grid[position] == '|') {
+                position += direction
+                steps++
+            }
+
+            val cell = grid[position]
+            direction = when {
+                (cell == '7' && direction == UP) || (cell == 'J' && direction == DOWN) -> LEFT
+                (cell == 'F' && direction == UP) || (cell == 'L' && direction == DOWN) -> RIGHT
+                cell == '7' || cell == 'F' -> DOWN
+                cell == 'J' || cell == 'L' -> UP
+                else -> {
+                    area(corner, position)
+                    break
                 }
             }
+
+            area(corner, position)
+            corner = Point(position)
+            position += direction
+            steps++
         }
-        neighbours[startCell] = neighbours.filter { (_, v) -> v.any { it == startCell } }.keys.toList()
-        return startCell to neighbours
+
+        return steps
     }
 
-    fun getMainLoopWithDistance(startCell: CharPoint, neighbours: Map<CharPoint, List<CharPoint>>): HashMap<CharPoint, Int> = bfs(startCell) { neighbours[it]!! }
-
     override fun part1(input: String): Int {
-        val (startCell, neighbours) = getNeighbours(input)
-        return getMainLoopWithDistance(startCell, neighbours).maxBy { it.value }.value
+        val steps = parse(input)
+        return steps / 2
     }
 
     override fun part2(input: String): Int {
-        val (startCell, neighbours) = getNeighbours(input)
-        val mainLoop = getMainLoopWithDistance(startCell, neighbours)
-        val grid = input.lines().toCharGrid()
-        val cleanMap = grid
-            .mapIndexed { y, row ->
-                row.mapIndexed { x, c -> CharPoint(y, x, c) }.map {
-                    if (it.data == 'S') {
-                        val n = neighbours[it]!!.map { it.data }.toSet()
-                        when {
-                            n == setOf('-') || n == setOf('F', '7') || n == setOf('L', 'J') -> '-'
-                            else -> '|'
-                        }
-                    } else if (it in mainLoop) {
-                        it.data
-                    } else {
-                        '.'
-                    }
-                }
-            }
-
-        var insideCount = 0
-
-        for (row in cleanMap) {
-            var parity = 0
-            var previous = '.'
-            for (char in row) {
-                when (char) {
-                    '.' -> if (parity % 2 != 0) insideCount++
-                    in "JLF7|" -> {
-                        when (previous to char) {
-                            'F' to 'J', 'L' to '7' -> Unit
-                            else -> parity++
-                        }
-                        previous = char
-                    }
-                }
-            }
+        var area = 0
+        val steps = parse(input) { corner, position ->
+            area += determinant(corner, position)
         }
-        return insideCount
+        return abs(area) / 2 - steps / 2 + 1
     }
 }
