76e99b6f5ac0ca146ecc7288b485b84f906d8d2b
Marek Kohn <me@makohn.de>
2026-05-10 18:18:37 +0200

    Refactor solution for 2021 day 15

--- src/year2021/Day15.kt
+++ src/year2021/Day15.kt
@@ -1,51 +1,65 @@
 package year2021
 
-import util.algorithm.*
 import util.core.*
 import util.grid.*
 import util.point.*
+import java.util.*
 
 class Day15 : Solution<Int, Int> {
 
+    private data class State(val point: Point, val cost: Int) : Comparable<State> {
+        override fun compareTo(other: State): Int = this.cost.compareTo(other.cost)
+    }
+
+    private fun dijkstra(grid: IntGrid): Int {
+        val (width, height) = grid.shape
+
+        val todo = PriorityQueue<State>()
+        val visited = HashMap<Point, Int>()
+        val start = Point(0, 0)
+        val end = Point(width - 1, height - 1)
+
+        todo.add(State(start, 0))
+        visited[start] = 0
+
+        while (todo.isNotEmpty()) {
+            val (point, cost) = todo.remove()
+            if (cost <= visited[point]!!) {
+                for (next in RDLU.map { it + point }) {
+                    if (next in grid) {
+                        val nextCost = grid[next] + cost
+                        if (next !in visited || nextCost < visited[next]!!) {
+                            visited[next] = nextCost
+                            todo.add(State(next, nextCost))
+                        }
+                    }
+                }
+            }
+        }
+
+        return visited[end]!!
+    }
+
     override fun part1(input: String): Int {
-        val board = parse(input)
-        val (width, height) = board.shape
-        return solve(board, width, height)
+        val grid = input.lines().toIntGrid()
+        return dijkstra(grid)
     }
 
     override fun part2(input: String): Int {
-        val board = parse(input)
-        val (width, height) = board.shape
-        val map = Array(height * 5) { IntArray(width * 5) }
-        for (i in board.indices) {
-            for (j in board[i].indices) {
+        val grid = input.lines().toIntGrid()
+        val (width, height) = grid.shape
+        val expanded = Array(height * 5) { IntArray(width * 5) }
+        for (i in grid.indices) {
+            for (j in grid[i].indices) {
                 for (k in 0..4) {
                     val l = j + k * width
-                    map[i][l] = (((board[i][j] + 1 * k - 1)) % 9) + 1
+                    expanded[i][l] = (((grid[i][j] + 1 * k - 1)) % 9) + 1
                 }
             }
             for (x in 1..4) {
-                map[i + x * height] = map[i].map { ((it + x * 1 - 1)) % 9 + 1 }.toIntArray()
+                expanded[i + x * height] = expanded[i].map { ((it + x * 1 - 1)) % 9 + 1 }.toIntArray()
             }
         }
-        return solve(map, width * 5, height * 5)
-    }
-
-    private fun parse(input: String) = input.lines().map { it.map { c -> c.digitToInt() }.toIntArray() }.toTypedArray()
-
-    private fun solve(board: Array<IntArray>, width: Int, height: Int): Int {
-        fun adjacent(u: Point) = buildList {
-            val (x, y) = u
-            for ((nx, ny) in listOf(Point(x, y - 1), Point(x, y + 1), Point(x - 1, y), Point(x + 1, y))) {
-                if ((0 <= ny) && (ny < width) && (0 <= nx) && (nx < height)) {
-                    add(Point(nx, ny) to board[ny][nx])
-                }
-            }
-        }
-
-        val start = Point(0, 0)
-        val end = Point(height - 1, width - 1)
-
-        return dijkstra(start, ::adjacent)[end]!!
+        return dijkstra(expanded)
     }
 }
